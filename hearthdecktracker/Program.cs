c6c230c9613f2b4348ba2ecff32c673b12e815b0
Jacob Zweifel <jakez834@gmail.com>
2014-05-22 16:21:48 -0500

    Cleaning house, added JSON data layer
    Cleaned up the code a little bit - moved many inline class declarations
    out to their own class files. Much easier to manage!
    
    Also, I scrapped the code that was declaring DeckLists and wrote up a
    few classes to read that data from a JSON file.

--- hearthdecktracker/Program.cs
+++ hearthdecktracker/Program.cs
@@ -16,7 +16,7 @@ namespace hearthdecktracker
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
